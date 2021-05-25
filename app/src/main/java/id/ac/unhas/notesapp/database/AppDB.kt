06ed2181c53cb25a910dd0830f031ef5252ea916
Nur Annisa Yusrah Putra Djaya <nurannisayusrahpdj@gmail.com>
2021-05-25 23:50:35 +0800

    Create NoteDao.kt

--- app/src/main/java/id/ac/unhas/notesapp/database/AppDB.kt
+++ app/src/main/java/id/ac/unhas/notesapp/database/AppDB.kt
@@ -4,6 +4,8 @@ import android.content.Context
 import androidx.room.Database
 import androidx.room.Room
 import androidx.room.RoomDatabase
+import id.ac.unhas.notesapp.database.note.Note
+import id.ac.unhas.notesapp.database.note.NoteDao
 
 @Database(entities = [Note::class], exportSchema = false, version = 1)
 abstract class AppDatabase : RoomDatabase() {
